0ad52299b8a92350c222420f4b5a174650f3a88b
iilei • jochen preusche <922226+iilei@users.noreply.github.com>
2024-12-03 07:40:44 +0100

    feat: Add CLI command 'cronspell-dot'

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,7 @@
-"""
-Dummy conftest.py for `CronSpell`.
+import pytest
 
-If you don't know what this is for, just leave it empty.
-Read more about conftest.py under:
-- https://docs.pytest.org/en/stable/fixture.html
-- https://docs.pytest.org/en/stable/writing_plugins.html
-"""
 
-# import pytest
+@pytest.fixture(scope="session")
+def data_path(tmp_path_factory):
+    fn = tmp_path_factory.mktemp("data")
+    return fn
